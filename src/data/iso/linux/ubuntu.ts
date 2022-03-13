db675bed16852af8b11c8c1b39ab0eb97ad303c1
zmaplex <zmaplex@gmail.com>
2022-03-13 17:38:03 +0800

    add ubuntu 16-21 lts

--- src/data/iso/linux/ubuntu.ts
+++ src/data/iso/linux/ubuntu.ts
@@ -17,7 +17,8 @@ const ubuntu: OSVersionIF = {
       'ED2K': '',
       'BoatCloud': '',
       'Extend': []
-    }, {
+    },
+    {
       'Meta': {
         'name': 'ubuntu-21.10-desktop-amd64.iso',
         'architecture': 'amd64',
@@ -32,8 +33,136 @@ const ubuntu: OSVersionIF = {
       'BoatCloud': '',
       'Extend': []
     },
-
   ],
+  '21.04': [
+    {
+      Meta: {
+        name: 'ubuntu-21.04-desktop-amd64.iso',
+        description: '',
+        size: '2.6 GiB',
+        at_date: '2021-04-20 19:16',
+        up_date: '2021-04-20 19:16',
+        architecture: 'amd64'
+
+      },
+      Direct: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/21.04/ubuntu-21.04-desktop-amd64.iso',
+      BoatCloud: '',
+      BitTorrent: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/21.04/ubuntu-21.04-desktop-amd64.iso.torrent',
+      ED2K: '',
+      Extend: []
+    },
+    {
+      Meta: {
+        name: 'ubuntu-21.04-live-server-amd64.iso',
+        description: '',
+        size: '1.1 GiB',
+        at_date: '2021-04-20 19:16',
+        up_date: '2021-04-20 19:16',
+        architecture: 'amd64'
+      },
+      Direct: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/21.04/ubuntu-21.04-live-server-amd64.iso',
+      BoatCloud: '',
+      BitTorrent: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/21.04/ubuntu-21.04-live-server-amd64.iso.torrent',
+      ED2K: '',
+      Extend: []
+    }
+  ],
+  '18.04': [
+    {
+      Meta: {
+        name: 'ubuntu-18.04.6-desktop-amd64.iso',
+        description: '',
+        size: '2.3 GiB',
+        at_date: '2021-04-20 19:16',
+        up_date: '2021-04-20 19:16',
+        architecture: 'amd64'
+
+      },
+      Direct: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/18.04/ubuntu-18.04.6-desktop-amd64.iso',
+      BoatCloud: '',
+      BitTorrent: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/18.04/ubuntu-18.04.6-desktop-amd64.iso.torrent',
+      ED2K: '',
+      Extend: []
+    },
+    {
+      Meta: {
+        name: 'ubuntu-18.04.6-live-server-amd64.iso',
+        description: '',
+        size: '969 MiB',
+        at_date: '2021-04-20 19:16',
+        up_date: '2021-04-20 19:16',
+        architecture: 'amd64'
+
+      },
+      Direct: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/18.04/ubuntu-18.04.6-live-server-amd64.iso',
+      BoatCloud: '',
+      BitTorrent: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/18.04/ubuntu-18.04.6-live-server-amd64.iso.torrent',
+      ED2K: '',
+      Extend: []
+    },
+  ],
+  '16.04': [
+    {
+      Meta: {
+        name: 'ubuntu-16.04.6-desktop-i386.iso',
+        description: '',
+        size: '1.6 GiB',
+        at_date: '2021-04-20 19:16',
+        up_date: '2021-04-20 19:16',
+        architecture: 'i386'
+      },
+      Direct: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/ubuntu-16.04.6-desktop-i386.iso',
+      BoatCloud: '',
+      BitTorrent: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/ubuntu-16.04.6-desktop-i386.iso.torrent',
+      ED2K: '',
+      Extend: []
+    },
+    {
+      Meta: {
+        name: 'ubuntu-16.04.6-server-i386.iso',
+        description: '',
+        size: '837 MiB',
+        at_date: '2021-04-20 19:16',
+        up_date: '2021-04-20 19:16',
+        architecture: 'i386'
+      },
+      Direct: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/ubuntu-16.04.6-server-i386.iso',
+      BoatCloud: '',
+      BitTorrent: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/ubuntu-16.04.6-server-i386.iso.torrent',
+      ED2K: '',
+      Extend: []
+    },
+    {
+      Meta: {
+        name: 'ubuntu-16.04.7-desktop-amd64.iso',
+        description: '',
+        size: '1.6 GiB',
+        at_date: '2021-04-20 19:16',
+        up_date: '2021-04-20 19:16',
+        architecture: 'amd64'
+      },
+      Direct: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/ubuntu-16.04.7-desktop-amd64.iso',
+      BoatCloud: '',
+      BitTorrent: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/ubuntu-16.04.7-desktop-amd64.iso.torrent',
+      ED2K: '',
+      Extend: []
+    },
+    {
+      Meta: {
+        name: 'ubuntu-16.04.7-server-amd64.iso',
+        description: '',
+        size: '880 MiB',
+        at_date: '2021-04-20 19:16',
+        up_date: '2021-04-20 19:16',
+        architecture: 'amd64'
+      },
+      Direct: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/ubuntu-16.04.7-server-amd64.iso',
+      BoatCloud: '',
+      BitTorrent: 'https://mirrors.tuna.tsinghua.edu.cn/ubuntu-releases/16.04/ubuntu-16.04.7-server-amd64.iso.iso.torrent',
+      ED2K: '',
+      Extend: []
+    },
+  ]
 
 }
 export default ubuntu
