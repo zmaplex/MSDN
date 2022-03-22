2008cd7178c81ffab960eb851ee33fb86c8c2313
zmaplex <zmaplex@gmail.com>
2022-03-22 15:18:03 +0800

    add windows 11

--- src/data/iso/windows/index.ts
+++ src/data/iso/windows/index.ts
@@ -1,8 +1,10 @@
 import windows from 'src/data/iso/windows/windows';
 import {DistributionIF} from 'src/data/iso';
+import windows11 from 'src/data/iso/windows/windows11';
 
 const windowsData: DistributionIF = {
   'windows XP': windows,
+  'windows 11': windows11
 
 }
 
---
+++ src/data/iso/windows/windows11.ts
@@ -0,0 +1,45 @@
+import {OSVersionIF} from 'src/data/iso';
+
+
+const windows11: OSVersionIF = {
+  'Windows 11 business': [
+    {
+      'Meta': {
+        'name': 'Windows 11 (business editions), (updated March 2022) (x64) - DVD (Chinese-Simplified)',
+        'architecture': 'x64',
+        'description': 'Windows 11 (business editions), (updated March 2022) (x64) - DVD (Chinese-Simplified).iso',
+        'size': '5.2 GiB',
+        'at_date': '2022-03-15 00:00',
+        'up_date': '2022-03-15 00:00'
+      },
+      'Direct': '',
+      'BitTorrent': 'magnet:?xt=urn:btih:59abaad8e68806ebac108bd69b13d7e9a38be5fb&dn=zh-cn_windows_11_business_editions_updated_march_2022_x64_dvd_7df6eae1.iso&xl=5582235648',
+      'ED2K': 'ed2k://|file|zh-cn_windows_11_business_editions_updated_march_2022_x64_dvd_7df6eae1.iso|5582235648|B7C9D076AE16A10C9D2610207577F28D|/',
+      'BoatCloud': '',
+      'Extend': []
+    }
+
+  ],
+
+  'Windows 11 consumer': [
+    {
+      'Meta': {
+        'name': 'Windows 11 (consumer editions), (updated March 2022) (x64) - DVD (Chinese-Simplified)',
+        'architecture': 'x64',
+        'description': 'Windows 11 (consumer editions), (updated March 2022) (x64) - DVD (Chinese-Simplified).iso',
+        'size': '5.3 GiB',
+        'at_date': '2022-03-15 00:00',
+        'up_date': '2022-03-15 00:00'
+      },
+      'Direct': '',
+      'BitTorrent': 'magnet:?xt=urn:btih:169ea0eed6852c53a3c46161f1a1762d96023c3e&dn=zh-cn_windows_11_consumer_editions_updated_march_2022_x64_dvd_07cc21aa.iso&xl=5692635136',
+      'ED2K': 'ed2k://|file|zh-cn_windows_11_consumer_editions_updated_march_2022_x64_dvd_07cc21aa.iso|5692635136|0C94072A76656EC92D34124A59115E2C|/',
+      'BoatCloud': '',
+      'Extend': []
+    }
+
+  ],
+
+}
+export default windows11
+
